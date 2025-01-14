07c821ac831499652cddb18e5616719141bab132
Pipatchies <ariane.bertaud@gmail.com>
2025-01-14 14:55:27 +0100

    🧭 feat: configure BeersCatalog and HomePage Route with React Router setup

---
+++ src/pages/beersCatalog.tsx
@@ -0,0 +1,10 @@
+const BeersCatalog = () => {
+  return (
+    <div>
+      <h1>Catalogue des bières</h1>
+      {/* Liste des bières ici */}
+    </div>
+  );
+};
+
+export default BeersCatalog;
---
+++ src/pages/homePage.tsx
@@ -0,0 +1,10 @@
+const HomePage = () => {
+    return (
+      <div>
+        <h1>Hello world !</h1>
+        {/* Contenu */}
+      </div>
+    );
+  };
+  
+  export default HomePage;
