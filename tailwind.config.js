5c003f62b29912d96f3f3034c4882909824fb2d4
Pipatchies <ariane.bertaud@gmail.com>
2025-01-15 17:20:58 +0100

    🖋️ feat: configure Oswald font in the app

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        oswald: ['Oswald', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
