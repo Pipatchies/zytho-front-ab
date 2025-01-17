73ef5f5b169ea7176e830f4f0b1e900747dcd8c6
Pipatchies <ariane.bertaud@gmail.com>
2025-01-17 10:10:55 +0100

    ✨ fix: add animation to BeerCard using Framer Motion

--- src/components/beerCard.tsx
+++ src/components/beerCard.tsx
@@ -1,26 +1,46 @@
+import { motion } from "framer-motion";
 import { BeerResBody } from "../interfaces/beerInterface";
+import Beer from '../assets/beers/punkIpa.png';
 import { Link } from "react-router-dom";
 
 export const BeerCard = ({ beer }: { beer: BeerResBody }) => {
+
   return (
-    <div className="max-w-sm rounded-lg border border-gray-200 bg-white shadow-md overflow-hidden hover:shadow-lg transition-shadow duration-300 ease-in-out">
-      {/* Image Placeholder */}
-      <div className="h-48 bg-gray-300">
-        {/* Si tu as une image de bière, tu peux la mettre ici, sinon utiliser un fond par défaut */}
+    <motion.div
+      className="relative max-w-sm overflow-hidden z-10"
+    >
+      {/* Conteneur de l'image de la bière */}
+      <div
+        className="relative shadow-md hover:shadow-lg transition-shadow duration-300 ease-in-out group"
+      >
+        <img
+          src={Beer}
+          alt={beer.name}
+          className="w-full h-full object-cover"
+        />
       </div>
-      <div className="p-4">
-        <h3 className="text-xl font-semibold text-gray-800">{beer.name}</h3>
-        <p className="text-gray-600 text-sm">{beer.description.substring(0, 100)}...</p>
-        <p className="mt-2 text-gray-700">
-          <strong>Type:</strong> {beer.abv}% ABV
-        </p>
-        <Link
-          to={`/beer/${beer.id}`}
-          className="mt-4 inline-block text-blue-600 hover:text-blue-800 font-medium text-sm"
+
+        {/* Informations de la bière */}
+        <motion.div
+          className={`absolute inset-0 p-4 flex flex-col justify-center items-center bg-yellow-600 bg-opacity-80 text-white duration-300 ease-in-out rounded-lg`}
+          initial={{ opacity: 0 }} 
+          whileHover={{ opacity: 1 }}
         >
-          Voir détails
+          <h3 className="text-3xl font-semibold font-oswald">{beer.name}</h3>
+          <p className="text-md mt-4 text-center font-nunito italic">{beer.description}</p>
+          <Link
+          to={`/beer/${beer.id}`}
+          className="mt-6 px-4 py-2 bg-white text-yellow-600 font-semibold rounded-lg shadow-md hover:bg-gray-900 hover:text-white transition-colors duration-300 font-nunito"
+        >
+          En savoir +
         </Link>
-      </div>
-    </div>
+        </motion.div>
+    </motion.div>
   );
 };
+
+
+
+
+
+
